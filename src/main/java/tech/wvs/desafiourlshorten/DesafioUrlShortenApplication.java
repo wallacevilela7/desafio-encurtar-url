941a3113e6266b916675b6fece094498cab5cea1
wallacevilela7 <wallace.vilela9@gmail.com>
2025-05-28 15:17:18 -0300

    feat: initialize Spring Boot application with Maven wrapper and basic project structure

---
+++ src/main/java/tech/wvs/desafiourlshorten/DesafioUrlShortenApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.desafiourlshorten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioUrlShortenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioUrlShortenApplication.class, args);
+    }
+
+}
